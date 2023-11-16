5fc8629a3ecdea83d1f98b886dfdf3071ada10c1
wiwsic <82441666+wiwsic@users.noreply.github.com>
2023-11-16 00:59:11 -0300

    Finish initial 16x16 grid

--- script.js
+++ script.js
@@ -4,22 +4,34 @@ let main = document.querySelector('.main');
 let canvas = document.querySelector('.canvas');
 let input = document.querySelector('.inputNumber');
 let confirmBtn = document.querySelector('.confirm');
-let gridNumber = 64;
+let gridNumber = 16;
+let numberOfCells = gridNumber * gridNumber;
 
-// Calculate the width/height and make a nested loop to create the grid
+// Calculate the width/height
 
 let canvasWidth = canvas.clientWidth;
 let canvasHeight = canvas.clientHeight;
 
-let numberDivX = canvasWidth / gridNumber;
+// Set the borderSize, and the w/h of each div (considering the grid number, canvas w/h and the border)
 
-/* for (let x = 0; x < gridNumber; x++) {
+let borderSize = 1;
+
+let widthDivX = (canvasWidth / gridNumber) - (borderSize * 2);
+let heightDivY = (canvasHeight / gridNumber) - (borderSize * 2);
+
+// Create the loop and add the newDiv as a child of canvas div
+
+for (let x = 0; x < numberOfCells; x++) {
 
     let newDiv = document.createElement('div');
+    newDiv.style.width = `${widthDivX}px`;
+    newDiv.style.height = `${heightDivY}px`;
+    newDiv.style.backgroundColor = 'red'
+    newDiv.style.border = 'solid 1px'
+    canvas.appendChild(newDiv);
 
+} 
 
-
-}  */
 
 
 
